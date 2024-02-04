a128e4547c3b2b69ff71ae1f1dcfa21ac6dd5be6
Ryan Prince <judascleric@gmail.com>
2024-02-04 04:55:37 +0000

    fix service install

--- install-service.sh
+++ install-service.sh
@@ -7,9 +7,11 @@ SERVICE_CONF_PATH=/etc/systemd/system/auto-connect-midi.service
 poetry install
 venvDir=$(poetry env info | grep '^Path' | head -n 1 | awk '{print $2}')
 sudo ln -fs "$venvDir/bin/auto-connect-midi" /usr/local/bin/auto-connect-midi
+sudo ln -fs "$venvDir/bin/auto-connect-midi-config" /usr/local/bin/auto-connect-midi-config
 sudo cp ./auto-connect-midi.service "$SERVICE_CONF_PATH"
 sudo sed -i 's/User=unset/User='"$(whoami)"'/g' "$SERVICE_CONF_PATH"
 sudo systemctl daemon-reload
+sudo systemctl enable auto-connect-midi
 sudo systemctl restart auto-connect-midi
 systemctl status auto-connect-midi
 
